fc8d4e96062e5e7918e4ec3f99b89757c0e3212f
Mallepally shashi kanth varma <mallepally.shashikanthh@yahoo.com>
2024-05-12 15:21:15 +0530

    Create newYearChaos.java

---
+++ newYearChaos.java
@@ -0,0 +1,161 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'minimumBribes' function below.
+     *
+     * The function accepts INTEGER_ARRAY q as parameter.
+     */
+
+    public static void minimumBribes(List<Integer> q) {
+    // Write your code here
+    
+    int[] swapsCount=new int[q.size()+1];
+    
+    //bubble sort
+    
+    boolean isSwapped=false;
+    do{
+        isSwapped=false;
+        for(int i=0; i<q.size()-1; i++){
+            if(q.get(i) > q.get(i+1)) {
+                swap(q,i,i+1);
+                isSwapped=true;
+                
+            if( swapsCount[q.get(i+1)]>=2 ){
+                System.out.println("Too chaotic"); 
+                return;
+                }
+            swapsCount[q.get(i+1)]++;
+            }
+        }
+    }while(isSwapped);
+
+    int totalSwaps=0;
+    for(int i: swapsCount) totalSwaps+=i;
+    System.out.println(totalSwaps);
+    return;
+
+    }
+    
+    public static void swap(List<Integer> arr, int i ,int j){
+        
+        int temp=arr.get(i);
+        arr.set(i, arr.get(j));
+        arr.set(j,temp);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> q = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                Result.minimumBribes(q);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+    }
+}
+
+/*
+It is New Year's Day and people are in line for the Wonderland rollercoaster ride. Each person wears a sticker indicating their initial position in the queue from  to . Any person can bribe the person directly in front of them to swap positions, but they still wear their original sticker. One person can bribe at most two others.
+
+Determine the minimum number of bribes that took place to get to a given queue order. Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.
+
+Example
+
+
+If person  bribes person , the queue will look like this: . Only  bribe is required. Print 1.
+
+
+Person  had to bribe  people to get to the current position. Print Too chaotic.
+
+Function Description
+
+Complete the function minimumBribes in the editor below.
+
+minimumBribes has the following parameter(s):
+
+int q[n]: the positions of the people after all bribes
+Returns
+
+No value is returned. Print the minimum number of bribes necessary or Too chaotic if someone has bribed more than  people.
+Input Format
+
+The first line contains an integer , the number of test cases.
+
+Each of the next  pairs of lines are as follows:
+- The first line contains an integer , the number of people in the queue
+- The second line has  space-separated integers describing the final state of the queue.
+
+Constraints
+
+Subtasks
+
+For  score 
+For  score 
+
+Sample Input
+
+STDIN       Function
+-----       --------
+2           t = 2
+5           n = 5
+2 1 5 3 4   q = [2, 1, 5, 3, 4]
+5           n = 5
+2 5 1 3 4   q = [2, 5, 1, 3, 4]
+Sample Output
+
+3
+Too chaotic
+Explanation
+
+Test Case 1
+
+The initial state:
+
+pic1(1).png
+
+After person  moves one position ahead by bribing person :
+
+pic2.png
+
+Now person  moves another position ahead by bribing person :
+
+pic3.png
+
+And person  moves one position ahead by bribing person :
+
+pic5.png
+
+So the final state is  after three bribing operations.
+
+Test Case 2
+
+No person can bribe more than two people, yet it appears person  has done so. It is not possible to achieve the input state.
+*/
